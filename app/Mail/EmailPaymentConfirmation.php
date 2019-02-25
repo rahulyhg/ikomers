c271266cd4c9881a2a740d5ed32ee7b5b575a11d
asepsholihin <asep.sholihin11@gmail.com>
2019-02-25 17:05:44 +0700

    payment confirmation email

---
+++ app/Mail/EmailPaymentConfirmation.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class EmailPaymentConfirmation extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    protected $order;
+
+    public function __construct($order)
+    {
+        //
+        $this->order = $order;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        //return $this->view('view.name');
+        return $this->view('mail.email-payment-confimation')->with([
+        'order' => $this->order
+        ]);
+    }
+}
