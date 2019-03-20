4cdcd7da2664baf77204ad440fc1a6e1117bc805
asepsholihin <asep.sholihin11@gmail.com>
2019-03-21 00:22:54 +0700

    error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+<style>
+img {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    margin: auto;
+    object-fit: contain;
+    width: 60%;
+    height: 60%;
+}
+</style>
+
+<img src="{{ asset('resources/views/frontend/images/404_error_page.png') }}" alt="Error Page">
